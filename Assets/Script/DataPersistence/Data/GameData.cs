2e885f0d3afb31e885cfd6ddb57a4d2db178d8dd
christianasejo5@gmail.com <christianasejo5@gmail.com>
2024-04-23 00:24:27 +0800

    Modified1

--- Assets/Script/DataPersistence/Data/GameData.cs
+++ Assets/Script/DataPersistence/Data/GameData.cs
@@ -16,7 +16,12 @@ public class GameData
         this.countText = 0;
         this.relocationCount = 0;
         playerPosition = new Vector3(50.523f, 7.424f, 50.631f);
-        objectToRelocatePosition = new Vector3(49.175f, 6.258f, 52.478f);
         agentPosition = new Vector3(67.79f, 1.776f, 79.06f);
+        objectToRelocatePosition = new Vector3(49.175f, 6.258f, 52.478f);
+    }
+
+    public void ResetAgentPosition(Vector3 newPosition)
+    {
+        agentPosition = newPosition;
     }
 }
